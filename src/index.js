78c2965a478f5b9c6685d2ba5f0bc4a5c4cc8d0f
An <65577527+AndyGoldmann@users.noreply.github.com>
2021-08-03 20:15:03 -0500

    Initializing HTML and JS documents

---
+++ src/index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    navigator.geolocation.getCurrentPosition(position =>{
+        const lon = position.coords.longitude;
+        const lat = position.coords.latitude;
+        getWeather(lon, lat);
+    });
+    
+});
+
+async function getWeather(lon, lat) {
+    const url = `https://weather-proxy.freecodecamp.rocks/api/current?lon=${lon}&lat=${lat}`
+    const data = await fetch(url)
+    const weather = await data.json()
+    const currentUnitCelsius = Math.round(weather.main.temp);
+    const tempUnit = 'C'
+    console.log(weather)
+
+    document.querySelector('#city').append(weather.name);
+    document.querySelector('#country').append(weather.sys.country);
+    document.querySelector('#temp').append(`${currentUnitCelsius} °`);
+    document.querySelector('#tempUnit').append(tempUnit);
+};
